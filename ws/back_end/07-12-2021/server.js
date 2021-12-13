7677aa1bd8fd2751474289cfd8b3255eb20090ca
Gabo984 <fernandezmendozagabriel1@gmail.com>
2021-12-13 14:07:53 +0100

    changes

---
+++ ws/back_end/07-12-2021/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const express = require("express")
+const server = express()
+
+server.listen(process.env.PORT)
+
+server.get("/", (req, res) => {
+    res.json({
+        msg: "still working"
+        
+    })
+})
